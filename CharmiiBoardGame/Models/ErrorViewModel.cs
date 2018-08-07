e01c7b22473768145f8752b51eaec33264b74a89
yuca <yuca@Ctrip.com>
2018-08-07 13:52:09 +0800

    添加项目文件。

---
+++ CharmiiBoardGame/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CharmiiBoardGame.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
